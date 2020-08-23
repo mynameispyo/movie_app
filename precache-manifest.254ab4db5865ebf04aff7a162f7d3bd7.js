3a94766380cb9e8359b45b0fbd6dc94825a298ea
Inpyo Lee <mynameispyo@gmail.com>
2020-08-22 22:06:28 -0400

    Updates

---
+++ precache-manifest.254ab4db5865ebf04aff7a162f7d3bd7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2b4362f0f813238da173097b6b16bfa2",
+    "url": "/movie_app/index.html"
+  },
+  {
+    "revision": "bb6f316e1ca3ac0b71fd",
+    "url": "/movie_app/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "2cb00f0b337cb1b1485d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app/static/js/2.03729dac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bb6f316e1ca3ac0b71fd",
+    "url": "/movie_app/static/js/main.3670ccb4.chunk.js"
+  },
+  {
+    "revision": "8a294428b8499a7b0c17",
+    "url": "/movie_app/static/js/runtime-main.9ac0a6e2.js"
+  }
+]);
